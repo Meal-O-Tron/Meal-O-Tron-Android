62561b80a3858a36b9ffa63767cb402eac9fa8ae
BuildTools <unconfigured@null.spigotmc.org>
2018-12-11 17:40:02 +0100

    ScreenStats - Networked dog weight

--- app/src/main/java/com/food/kuruyia/foodretriever/mainscreen/ScreenStats.java
+++ app/src/main/java/com/food/kuruyia/foodretriever/mainscreen/ScreenStats.java
@@ -9,30 +9,29 @@ import com.food.kuruyia.foodretriever.utils.DataType;
 import com.food.kuruyia.foodretriever.utils.IDataChange;
 import com.food.kuruyia.foodretriever.utils.IFabInteract;
 import com.food.kuruyia.foodretriever.R;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class ScreenStats extends Fragment implements IFabInteract, IDataChange {
+    GraphView m_graphDogWeight;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.screen_stats, container, false);
 
-        GraphView graphDogWeight = view.findViewById(R.id.graphDogWeight);
-
-        LineGraphSeries<DataPoint> pointsDogWeight = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(1, 1),
-                new DataPoint(2, 2),
-                new DataPoint(3, 3)
-        });
-        pointsDogWeight.setDrawBackground(true);
-        graphDogWeight.addSeries(pointsDogWeight);
+        m_graphDogWeight = view.findViewById(R.id.graphDogWeight);
 
         return view;
     }
@@ -49,7 +48,30 @@ public class ScreenStats extends Fragment implements IFabInteract, IDataChange {
 
     @Override
     public void onDataChanged(DataType dataType, JsonObject data) {
+        switch (dataType) {
+            case DATA_STATS_WEIGHT: {
+                if (data.has("values")) {
+                    JsonArray values = data.getAsJsonArray("values");
 
+                    List<DataPoint> list = new ArrayList<>();
+                    for (int i = 0; i < values.size(); i++) {
+                        list.add(new DataPoint(i, values.get(i).getAsFloat()));
+                    }
+
+                    DataPoint[] dp = new DataPoint[list.size()];
+                    dp = list.toArray(dp);
+                    LineGraphSeries<DataPoint> pointsDogWeight = new LineGraphSeries<>(dp);
+                    pointsDogWeight.setDrawBackground(true);
+                    pointsDogWeight.setDrawDataPoints(true);
+
+                    m_graphDogWeight.removeAllSeries();
+                    m_graphDogWeight.addSeries(pointsDogWeight);
+                    m_graphDogWeight.invalidate();
+                }
+
+                break;
+            }
+        }
     }
 
     @Override
